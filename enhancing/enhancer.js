342a6e700d0873c73907c557820851ba450f295e
Anthony M Hernandez <55599397+asvka@users.noreply.github.com>
2020-05-14 12:56:12 -0700

    MVP!

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -23,12 +23,12 @@ function fail(item) {
   if (item.enhancement < 15) {
     item.durability = item.durability - 5;
   } 
-    // else if (item.enhancement > 15) {
-    // item.durability - 10;
-    //   } 
-  //     else if (item.enhancement > 16) {
-  //   item.durability--;
-  //         };
+    else if (item.enhancement > 15) {
+    item.durability = item.durability - 10;
+      } 
+      else if (item.enhancement > 16) {
+    item.durability = item.durability--;
+          };
   return { ...item };
 }
 
